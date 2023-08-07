a0f10249ff95acd6325b7530fbdd45245c9c6e7e
Leeseojune <sung07288346@gmail.com>
2023-08-08 00:12:23 +0900

    Root Application

---
+++ SimpleFramework/src/RootApplication.java
@@ -0,0 +1,17 @@
+import db.SessionManager;
+
+public class RootApplication {
+
+    public static void main(String[] args) {
+
+
+        // service layer
+
+        SessionManager.getSessionManager().get().getTransaction().begin();
+
+        // business logic
+
+        SessionManager.getSessionManager().get().getTransaction().commit();
+    }
+
+}
