a1d9ec100ff57aa23a45ef6b4f801ba1a3a91cd6
Leeseojune <sung07288346@gmail.com>
2023-08-12 23:19:59 +0900

    Transactional annotation restrict use only method

--- src/main/java/org/example/annotataion/Transactional.java
+++ src/main/java/org/example/annotataion/Transactional.java
@@ -1,4 +1,9 @@
 package org.example.annotataion;
 
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Target({METHOD})
 public @interface Transactional {
 }
